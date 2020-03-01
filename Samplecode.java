cbc817331f44c2d9ce81eb721df0a237abb81ec3
darshansbg <55429324+darshansbg@users.noreply.github.com>
2020-03-01 17:47:32 +0530

    Update Samplecode.java

--- Samplecode.java
+++ Samplecode.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]) throws InterruptedException{  
      Thread.sleep(5000);
-     System.out.println("Hello Java updated on the master branch - 28th Jan");  
+     System.out.println("Hello Java updated on the Branch Feb 3rd 2020");  
     }  
 }
